dc516c2bb02acb56868a91e913dfc2b0abb0594c
jceso <01gabrielecesali@gmail.com>
2024-11-22 19:55:29 +0100

    CRUD but save doesn't work

---
+++ app/src/main/java/com/example/myapplication/EventAdapter.java
@@ -0,0 +1,67 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NonBlocking;
+
+import java.util.ArrayList;
+
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> {
+    Context context;
+    ArrayList<Event> arrayList;
+    OnItemClickListener onItemClickListener;
+
+    public EventAdapter(Context context, ArrayList<Event> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.event_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.title.setText(arrayList.get(position).getTitle());
+        holder.subtitle.setText(arrayList.get(position).getSubtitle());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onItemClickListener.onItemClick(arrayList.get(position));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title, subtitle;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.list_title);
+            subtitle = itemView.findViewById(R.id.list_subtitle);
+        }
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Event event);
+    }
+}

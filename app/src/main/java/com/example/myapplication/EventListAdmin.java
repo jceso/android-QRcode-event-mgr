e6492789cbc1946af8007120c6f51fda190e3cd8
jceso <01gabrielecesali@gmail.com>
2024-11-25 16:54:11 +0100

    Edit and save work, but you can leave empty parameters

--- app/src/main/java/com/example/myapplication/EventListAdmin.java
+++ app/src/main/java/com/example/myapplication/EventListAdmin.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
@@ -44,11 +45,16 @@ public class EventListAdmin extends AppCompatActivity {
             return insets;
         });
 
+        // Firebase setting
         FirebaseApp.initializeApp(EventListAdmin.this);
         database = FirebaseDatabase.getInstance("https://ing-soft-firebase-default-rtdb.europe-west1.firebasedatabase.app/");
 
-
+        // Buttons and views
         FloatingActionButton addButton = findViewById(R.id.addButton);
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        TextView empty = findViewById(R.id.empty_list);
+
+        // CRUD setting
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,116 +62,7 @@ public class EventListAdmin extends AppCompatActivity {
             }
         });
 
-        TextView empty = findViewById(R.id.empty_list);
-        RecyclerView recyclerView = findViewById(R.id.recycler);
-
-        database.getReference().child("event").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                ArrayList<Event> arrayList = new ArrayList<>();
-
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                    Event event = dataSnapshot.getValue(Event.class);
-                    Objects.requireNonNull(event).setKey(dataSnapshot.getKey());
-                    arrayList.add(event);
-                }
-                if (arrayList.isEmpty()) {
-                    recyclerView.setVisibility(View.GONE);
-                    empty.setVisibility(View.VISIBLE);
-                } else {
-                    recyclerView.setVisibility(View.VISIBLE);
-                    empty.setVisibility(View.GONE);
-                }
-
-                EventAdapter adapter = new EventAdapter(EventListAdmin.this, arrayList);
-                recyclerView.setAdapter(adapter);
-                adapter.setOnItemClickListener(new EventAdapter.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(Event event) {
-                        View view = LayoutInflater.from(EventListAdmin.this).inflate(R.layout.add_event_dialog, null);
-
-                        TextView title = view.findViewById(R.id.titleET);
-                        TextView subtitle = view.findViewById(R.id.subtitleET);
-                        TextView desc = view.findViewById(R.id.descET);
-
-                        title.setText(event.getTitle());
-                        subtitle.setText(event.getSubtitle());
-                        desc.setText(event.getDescription());
-
-                        ProgressDialog progressDialog = new ProgressDialog(EventListAdmin.this);
-
-                        AlertDialog alertDialog = new AlertDialog.Builder(EventListAdmin.this)
-                                .setTitle("Edit")
-                                .setView(view)
-                                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialogInterface, int which) {
-                                        if (title.getText().toString().isEmpty())
-                                            title.setError("Title is required");
-                                        else if (subtitle.getText().toString().isEmpty())
-                                            subtitle.setError("Subtitle is required");
-                                        else if (desc.getText().toString().isEmpty())
-                                            desc.setError("Description is required");
-
-                                        progressDialog.setMessage("Saving...");
-                                        progressDialog.show();
-
-                                        Event event = new Event();
-                                        event.setTitle(title.getText().toString());
-                                        event.setSubtitle(subtitle.getText().toString());
-                                        event.setDescription(desc.getText().toString());
-
-                                        database.getReference().child("event").child(event.getKey()).setValue(event).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                            @Override
-                                            public void onSuccess(Void unused) {
-                                                progressDialog.dismiss();
-                                                dialogInterface.dismiss();
-                                                Toast.makeText(EventListAdmin.this, "Saved successfully", Toast.LENGTH_SHORT).show();
-                                            }
-                                        }).addOnFailureListener(new OnFailureListener() {
-                                            @Override
-                                            public void onFailure(@NonNull Exception e) {
-                                                progressDialog.dismiss();
-                                                Toast.makeText(EventListAdmin.this, "There was an error while saving data", Toast.LENGTH_SHORT).show();
-                                            }
-                                        });
-                                    }
-                                }).setNeutralButton("Close", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialogInterface, int which) {
-                                        dialogInterface.dismiss();
-                                    }
-                                }).setNegativeButton("Delete", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialogInterface, int which) {
-                                        progressDialog.setMessage("Deleting...");
-                                        progressDialog.show();
-
-                                        database.getReference().child("event").child(event.getKey()).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
-                                            @Override
-                                            public void onSuccess(Void unused) {
-                                                progressDialog.dismiss();
-                                                Toast.makeText(EventListAdmin.this, "Deleted successfully", Toast.LENGTH_SHORT).show();
-                                            }
-                                        }).addOnFailureListener(new OnFailureListener() {
-                                            @Override
-                                            public void onFailure(@NonNull Exception e) {
-                                                progressDialog.dismiss();
-                                                Toast.makeText(EventListAdmin.this, "There was an error while deleting data", Toast.LENGTH_SHORT).show();
-                                            }
-                                        });
-                                    }
-                                }).create();
-                        alertDialog.show();
-                    }
-                });
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
+        editDeleteEvent(recyclerView, empty);
 
     }
 
@@ -183,10 +80,12 @@ public class EventListAdmin extends AppCompatActivity {
                     public void onClick(DialogInterface dialogInterface, int which) {
                         if (title.getText().toString().isEmpty())
                             title.setError("Title is required");
-                        else if (subtitle.getText().toString().isEmpty())
+                        if (subtitle.getText().toString().isEmpty())
                             subtitle.setError("Subtitle is required");
-                        else if (desc.getText().toString().isEmpty())
+                        if (desc.getText().toString().isEmpty())
                             desc.setError("Description is required");
+
+                        Log.d("AddEvent", "desc: " + desc.getText().toString() + "so desc is empty? " + desc.getText().toString().isEmpty());
 
                         ProgressDialog dialog = new ProgressDialog(EventListAdmin.this);
                         dialog.setMessage("Adding event...");
@@ -222,8 +121,7 @@ public class EventListAdmin extends AppCompatActivity {
         alertDialog.show();
     }
 
-    /*
-    private void editDeleteEvent(RecyclerView recyclerView) {
+    private void editDeleteEvent(RecyclerView recyclerView, TextView empty) {
         database.getReference().child("event").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -244,6 +142,8 @@ public class EventListAdmin extends AppCompatActivity {
 
                 EventAdapter adapter = new EventAdapter(EventListAdmin.this, arrayList);
                 recyclerView.setAdapter(adapter);
+
+                Log.d("EditDeleteEvent", "Adapter set");
                 adapter.setOnItemClickListener(new EventAdapter.OnItemClickListener() {
                     @Override
                     public void onItemClick(Event event) {
@@ -267,20 +167,20 @@ public class EventListAdmin extends AppCompatActivity {
                                     public void onClick(DialogInterface dialogInterface, int which) {
                                         if (title.getText().toString().isEmpty())
                                             title.setError("Title is required");
-                                        else if (subtitle.getText().toString().isEmpty())
+                                        if (subtitle.getText().toString().isEmpty())
                                             subtitle.setError("Subtitle is required");
-                                        else if (desc.getText().toString().isEmpty())
+                                        if (desc.getText().toString().isEmpty())
                                             desc.setError("Description is required");
 
                                         progressDialog.setMessage("Saving...");
                                         progressDialog.show();
 
-                                        Event event = new Event();
-                                        event.setTitle(title.getText().toString());
-                                        event.setSubtitle(subtitle.getText().toString());
-                                        event.setDescription(desc.getText().toString());
+                                        Event eventUpd = new Event();
+                                        eventUpd.setTitle(title.getText().toString());
+                                        eventUpd.setSubtitle(subtitle.getText().toString());
+                                        eventUpd.setDescription(desc.getText().toString());
 
-                                        database.getReference().child("event").child(event.getKey()).setValue(event).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        database.getReference().child("event").child(event.getKey()).setValue(eventUpd).addOnSuccessListener(new OnSuccessListener<Void>() {
                                             @Override
                                             public void onSuccess(Void unused) {
                                                 progressDialog.dismiss();
@@ -332,6 +232,4 @@ public class EventListAdmin extends AppCompatActivity {
             }
         });
     }
-
-    */
 }
